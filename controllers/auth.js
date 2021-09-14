01864b9c6217da48b0208f7479395c45a5e908fe
cortesnicolas <cortesnicolas006@gmail.com>
2021-09-13 22:16:36 -0300

    llamado al package

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,10 +37,58 @@ exports.registro = async (req, res) => {
   try {
     let user = req.body;
     const cone = await openBD();
-    cone.execute(
-      `INSERT INTO USUARIO(IDUSUARIO,RUT,NOMBRE,APELLIDOS,EMAIL,CONTRASENIA,ACTIVIDAD,DIRECCION,TELEFONO,IDROL) 
-      VALUES 
-      (${user.idUsuario},
+
+    sql = `begin PKG_METODOS.INSERTAR_USUARIO(
+      :V_RESPUESTA,
+      :return_code,
+      :return_message,
+      :V_RUT,
+      :V_NOMBRE,
+      :V_APELLIDOS ,
+      :V_EMAIL,
+      :V_CONTRASENIA,
+      :V_ACTIVIDAD,
+      :V_DIRECCION,
+      :V_TELEFONO,
+      :V_ROL); end;`;
+      
+    const data = {
+      V_RESPUESTA: 0,
+      return_code: 0,
+      return_message: '',
+      V_RUT: user.rut,
+      V_NOMBRE: user.nombre,
+      V_APELLIDOS: user.apellidos,
+      V_EMAIL: user.email,
+      V_CONTRASENIA: user.contrasenia,
+      V_ACTIVIDAD: '1',
+      V_DIRECCION: user.direccion,
+      V_TELEFONO: user.telefono,
+      V_ROL: user.idRol,
+    };
+
+    const binds = Object.assign({}, data);
+    result = cone.execute(sql, binds,
+      async (err, response) => {
+        await closeBD(cone);
+        if (err) {
+          console.log(err);
+          res.json({
+            success: false,
+            msg: "" + err,
+            user
+          });
+        }
+        if (response)
+          res.json({
+            success: true,
+            msg: "Usuario Creado Correctamente: "
+          });
+      });
+    console.log(result)
+    /* cone.execute(
+      `CALL PKG_METODOS.INSERTAR_USUARIO
+      (
         '${user.rut}',
         '${user.nombre}',
         '${user.apellidos}',
@@ -56,15 +104,16 @@ exports.registro = async (req, res) => {
           console.log(err)
           res.json({
             success:false,
-            msg:""+err
+            msg:""+err,
+            user
           });
         }
         if (response) res.json({
           success:true,
-          msg:"Usuario Creado Correctamente"
+          msg:"Usuario Creado Correctamente: " 
         });
-      }
-    );
+      } 
+    );*/
   } catch (error) {
     return res.json(error);
   }
