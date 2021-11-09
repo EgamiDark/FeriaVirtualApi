a5f6528d917f254f6b44a57abe80ebd71ad3c0b7
cortesnicolas <cortesnicolas006@gmail.com>
2021-11-09 01:26:37 -0300

    get estados de un pago

--- controllers/datosFk.js
+++ controllers/datosFk.js
@@ -175,3 +175,73 @@ exports.getEstadoOferta = async (req, res) => {
     return res.json(error);
   }
 };
+
+//CONSULTAR ESTADO OFERTA
+exports.getEstadoOferta = async (req, res) => {
+  try {
+    const cone = await openBD();
+    sql = `begin PKG_METODOS.OBTENER_EST_OFERTA(:cursor); end;`;
+
+    const data = {
+      cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
+    };
+
+    const result = await cone.execute(sql, data);
+    const resultSet = result.outBinds.cursor;
+
+    const rows = await resultSet.getRows();
+
+    if(rows){
+      res.json({
+        success: true,
+        msg: "Estado oferta obtenido correctamente",
+        rows,
+      });
+    }else{
+      res.json({
+        success: false,
+        msg: "" + err,
+        errorNum: err.errorNum,
+      });
+    }
+
+    await closeBD(cone);
+  } catch (error) {
+    return res.json(error);
+  }
+};
+
+//CONSULTAR ESTADO PAGO
+exports.getEstadoPago = async (req, res) => {
+  try {
+    const cone = await openBD();
+    sql = `begin PKG_METODOS.OBTENER_EST_PAGO(:cursor); end;`;
+
+    const data = {
+      cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
+    };
+
+    const result = await cone.execute(sql, data);
+    const resultSet = result.outBinds.cursor;
+
+    const rows = await resultSet.getRows();
+
+    if(rows){
+      res.json({
+        success: true,
+        msg: "Estados pago obtenido correctamente",
+        rows,
+      });
+    }else{
+      res.json({
+        success: false,
+        msg: "" + err,
+        errorNum: err.errorNum,
+      });
+    }
+
+    await closeBD(cone);
+  } catch (error) {
+    return res.json(error);
+  }
+};
--- routes/datosFK.js
+++ routes/datosFK.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { getTipoRefrigeracion,getTipoTransporte, getEstadoPedido, getEstadoSubasta, getEstadoOferta } = require('../controllers/datosFk');
+const {
+  getTipoRefrigeracion,
+  getTipoTransporte,
+  getEstadoPedido,
+  getEstadoSubasta,
+  getEstadoOferta,
+  getEstadoPago
+} = require("../controllers/datosFk");
 
-router.get('/tRefrigeracion', getTipoRefrigeracion);
-router.get('/tTransporte', getTipoTransporte);
-router.get('/estPedido', getEstadoPedido);
-router.get('/estSubasta', getEstadoSubasta);
-router.get('/estOferta', getEstadoOferta);
+router.get("/tRefrigeracion", getTipoRefrigeracion);
+router.get("/tTransporte", getTipoTransporte);
+router.get("/estPedido", getEstadoPedido);
+router.get("/estSubasta", getEstadoSubasta);
+router.get("/estOferta", getEstadoOferta);
+router.get("/estPago", getEstadoPago);
 
 module.exports = router;
