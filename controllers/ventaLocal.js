79e0e94b95ffeebd3a1adaa393f955948de18686
cortesnicolas <cortesnicolas006@gmail.com>
2021-11-19 19:22:49 -0300

    ventas locales

--- controllers/ventaLocal.js
+++ controllers/ventaLocal.js
@@ -48,7 +48,7 @@ exports.getVentasLocalesUsuario = async (req, res) => {
 
     const data = {
       cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
-      V_ID_USUARIO: req.params.idUsuario
+      V_ID_USUARIO: req.params.idUsuario,
     };
 
     const result = await cone.execute(sql, data);
@@ -185,6 +185,48 @@ exports.getVentasLocalesS = async (req, res) => {
         success: false,
         msg: "" + err,
         errorNum: err.errorNum,
+      });
+    }
+
+    await closeBD(cone);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// OBTIENE TODAS LAS VENTAS LOCALES SIN SUBASTAS O CON SUBASTAS CANCELADAS
+exports.getOfertasProductos = async (req, res) => {
+  try {
+    const cone = await openBD();
+    sql = `begin PKG_METODOS.OFERTAS_PRODUCTOS(:cursor); end;`;
+
+    const data = {
+      cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
+    };
+
+    const result = await cone.execute(sql, data);
+    const resultSet = result.outBinds.cursor;
+
+    const rows = await resultSet.getRows();
+
+    if (rows) {
+      if (rows.length > 0) {
+        res.json({
+          success: true,
+          msg: "Ofertas productos obtenidas correctamente",
+          rows
+        });
+      } else {
+        res.json({
+          success: false,
+          msg: "No hay ofertas productos"
+        });
+      }
+    } else {
+      res.json({
+        success: false,
+        msg: "" + err,
+        errorNum: err.errorNum
       });
     }
 
--- routes/ventaLocal.js
+++ routes/ventaLocal.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 // Metodos del controlador
-const { getOfertasVentaLocal, postVentaLocal, putEstOfertaP, getVentasLocalesS,getVentasLocalesUsuario } = require("../controllers/ventaLocal");
+const {
+  getOfertasVentaLocal,
+  postVentaLocal,
+  putEstOfertaP,
+  getVentasLocalesS,
+  getVentasLocalesUsuario,
+  getOfertasProductos
+} = require("../controllers/ventaLocal");
 
 // Rutas
 router.get("/ofertasVL", getOfertasVentaLocal);
@@ -10,5 +17,6 @@ router.get("/aSubastar", getVentasLocalesS);
 router.post("/guardar", postVentaLocal);
 router.post("/ofertaVL/aceptada", putEstOfertaP);
 router.get("/todos/usuario/:idUsuario", getVentasLocalesUsuario);
+router.get("/todos/ofertasProductos", getOfertasProductos);
 
 module.exports = router;
